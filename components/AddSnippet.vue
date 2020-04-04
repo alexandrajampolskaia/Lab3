a117c81cc545de0a9586f365b9f1c85988431a73
alexandrajampolskaia <a.jampolskaia@gmail.com>
2020-04-04 22:43:19 +0200

    deploy

---
+++ components/AddSnippet.vue
@@ -0,0 +1,115 @@
+<template>
+  <div>
+    <form>
+      <div>
+        <br />
+        <h1>/ADD SNIPPET/</h1>
+        <br />
+
+        <div>
+          <label>Name</label> &nbsp;
+          <input class="name" type="text" size="50" v-model="title" />
+        </div>
+
+        <br />
+        <div>
+          <label>Message</label> &nbsp;
+          <textarea class="message" type="text" rows="4" cols="4" v-model="content" />
+        </div>
+      </div>
+
+      <br />
+      <button @click="addToAPI">Submit</button>
+    </form>
+    <br />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "AddSnippet",
+  props: {
+    msg: String
+  },
+  data: () => ({
+    title: "",
+    content: ""
+  }),
+
+  methods: {
+    //ADD SNIPPET
+    addToAPI() {
+      let newSnippet = new URLSearchParams();
+      newSnippet.append("add", null);
+      newSnippet.append("title", this.title);
+      newSnippet.append("content", this.content);
+      newSnippet.append("tags", null);
+
+      axios({
+        method: "post",
+        url: "https://www.forverkliga.se/JavaScript/api/api-snippets.php",
+        data: newSnippet
+      })
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+h1 {
+  color: black;
+  background-color: white;
+}
+input {
+  padding: 10px;
+  outline: none;
+  margin-right: 130px;
+  font-size: 1em;
+}
+
+label {
+  color: white;
+  font-size: 1.3em;
+  margin-right: 50px;
+}
+
+.name {
+  width: 150px;
+}
+
+.message {
+  width: 350px;
+  padding: 10px;
+  outline: none;
+  margin-right: 130px;
+  font-size: 1em;
+}
+
+button {
+  padding: 10px;
+  font-size: 1em;
+  color: white;
+  border-radius: 2px;
+  background-color: black;
+  border: 2px solid white;
+}
+
+button:hover {
+  background-color: white;
+  color: black;
+}
+
+button:focus {
+  outline-width: 3px;
+  outline-color: gray;
+}
+</style>
---
+++ components/DeleteReport.vue
@@ -0,0 +1,203 @@
+<template>
+  <div class="snippetBox">
+    <AddSnippet class="addSnippetBox" />
+    <ShowSnippets
+      class="showSnippetsBox"
+      @theId="deleteSnippet($event)"
+      @reportId="reportSnippet($event)"
+      @upvoteId="upvoteSnippet($event)"
+      @downvotevoteId="downvoteSnippet($event)"
+      @reportOnId="unReport($event)"
+      @deleteReported="deleteReported($event)"
+    />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import AddSnippet from "./AddSnippet.vue";
+import ShowSnippets from "./ShowSnippets.vue";
+
+export default {
+  name: "DeleteReport",
+  components: {
+    AddSnippet,
+    ShowSnippets
+  },
+  props: {
+    msg: String
+  },
+
+  data: () => ({
+    snippet: []
+  }),
+
+  methods: {
+    deleteSnippet(emittedId) {
+      this.idToDelete = emittedId;
+      console.log(this.idToDelete);
+
+      let delSnippet = new URLSearchParams();
+      delSnippet.append("delete", null);
+      delSnippet.append("id", this.idToDelete);
+
+      axios({
+        method: "post",
+        url: "https://www.forverkliga.se/JavaScript/api/api-snippets.php",
+        data: delSnippet
+      })
+        .then(response => {
+          console.log(response);
+          this.snippet = this.snippet.filter(
+            snippet => snippet.id != this.idToDelete
+          );
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+
+    unReport(emittedId) {
+      this.idToUnreport = emittedId;
+      console.log(this.idToUnreport);
+
+      let unreportSnippet = new URLSearchParams();
+      unreportSnippet.append("unreport", null);
+      unreportSnippet.append("id", this.idToUnreport);
+
+      axios({
+        method: "post",
+        url: "https://www.forverkliga.se/JavaScript/api/api-snippets.php",
+        data: unreportSnippet
+      })
+        .then(response => {
+          console.log(response);
+          this.snippet = this.snippet.filter(
+            snippet => snippet.id != this.idToUnreport
+          );
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+
+    deleteReported(emittedId) {
+      this.idToDelete = emittedId;
+      console.log(this.idToDelete);
+
+      let delSnippet = new URLSearchParams();
+      delSnippet.append("delete", null);
+      delSnippet.append("id", this.idToDelete);
+
+      axios({
+        method: "post",
+        url:
+          "https://www.forverkliga.se/JavaScript/api/api-snippets.php?reported",
+        data: delSnippet
+      })
+        .then(response => {
+          console.log(response);
+          this.snippet = this.snippet.filter(
+            snippet => snippet.id != this.idToDelete
+          );
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    reportSnippet(emittedId) {
+      this.idToReport = emittedId;
+      console.log(this.idToReport);
+
+      let repSnippet = new URLSearchParams();
+      repSnippet.append("report", null);
+      repSnippet.append("id", this.idToReport);
+
+      axios({
+        method: "post",
+        url: "https://www.forverkliga.se/JavaScript/api/api-snippets.php",
+        data: repSnippet
+      })
+        .then(response => {
+          console.log(response);
+          this.snippet = this.snippet.filter(
+            snippet => snippet.id != this.idToReport
+          );
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    upvoteSnippet(emittedId) {
+      this.idToReport = emittedId;
+      console.log(this.idToReport);
+
+      let repSnippet = new URLSearchParams();
+      repSnippet.append("upvote", null);
+      repSnippet.append("id", this.idToReport);
+
+      axios({
+        method: "post",
+        url: "https://www.forverkliga.se/JavaScript/api/api-snippets.php",
+        data: repSnippet
+      })
+        .then(response => {
+          console.log(response);
+          this.snippet = this.snippet.filter(
+            snippet => snippet.id != this.idToReport
+          );
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    downvoteSnippet(emittedId) {
+      this.idToReport = emittedId;
+      console.log(this.idToReport);
+
+      let repSnippet = new URLSearchParams();
+      repSnippet.append("downvote", null);
+      repSnippet.append("id", this.idToReport);
+
+      axios({
+        method: "post",
+        url: "https://www.forverkliga.se/JavaScript/api/api-snippets.php",
+        data: repSnippet
+      })
+        .then(response => {
+          console.log(response);
+          this.snippet = this.snippet.filter(
+            snippet => snippet.id != this.idToReport
+          );
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }
+};
+</script>
+
+<style>
+.snippetBox {
+  display: grid;
+  grid-template-columns: 2;
+  grid-template-rows: 2;
+  grid-gap: 10px;
+  color: black;
+}
+
+.addSnippetBox {
+  grid-row: 1/2;
+  grid-column: 1/3;
+  margin: 20px;
+  background-color: black;
+}
+
+.showSnippetsBox {
+  grid-row: 2/3;
+  grid-column: 1/3;
+  border: 5px solid black;
+  margin: 0 20px 0 20px;
+}
+</style>
